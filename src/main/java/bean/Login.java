693188886485f6adfc291bc738b0d3ab0a608861
cat110779 <cat110779@gmail.com>
2016-08-16 17:17:51 +0800

    test

---
+++ src/main/java/bean/Login.java
@@ -0,0 +1,57 @@
+package bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import dao.UserDAO;
+import dao.UserDAOImpl;
+import model.Member;
+import model.User;
+
+@ManagedBean(name = "testBean")
+@SessionScoped
+public class Login implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private User user = new User();
+	private String message;
+
+	public String validateUsernamePassword() {
+		
+		System.out.println("帳號 : " + user.getAccount());
+		System.out.println("密碼 : " + user.getPassword());
+		
+		UserDAO dao = new UserDAOImpl();
+		
+		Member member = dao.getUserByAccount(user.getAccount(), user.getPassword());
+		
+		if(member != null) {
+			setMessage("");
+			return "shan2";
+		} else {
+			setMessage("帳號密碼錯誤，請重新登入");
+			return "shan";
+		}
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
---
+++ src/main/java/controller/ManjorBean.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+@SessionScoped @Named
+public class ManjorBean {
+	
+	public void login() {
+		System.out.println("test");
+	}
+}
