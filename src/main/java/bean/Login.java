ff3052a0dca352ab847c14dd37cefa48797db2bb
cat <cat@192.168.201.168>
2016-08-30 11:35:13 +0800

    test

--- src/main/java/bean/Login.java
+++ src/main/java/bean/Login.java
@@ -1,6 +1,5 @@
 package bean;
 
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
@@ -8,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.StringUtils;
 import org.greenpeace.utils.RandomPassword;
+import org.primefaces.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import dao.UserDAO;
@@ -19,32 +19,18 @@ import service.Email02;
 /**
  * The Class LoginController.
  */
-@ManagedBean(name = "logintest")
+@ManagedBean(name = "test")
 public class Login {
+	
+	
 
 	/** The Constant LOGGER. */
 	private static final Logger LOGGER = LoggerFactory.getLogger(Login.class);
 
 	/** The user. */
 	private User user = new User();
-	private String checkout;
 	/** The message. */
 	private String message;
-
-	public void forgottenPW(ActionEvent actionEvent){
-		LOGGER.debug("username: {}", user.getAccount());
-		LOGGER.debug("email: {}", user.getEmail());
-		
-		
-		String pw =  new RandomPassword().generate();
-		LOGGER.debug("random : {}",pw);
-		
-		//send email
-		new Email02( user.getEmail(), user.getAccount(), pw);
-
-		 //reset user data
-		 this.user=new User();
-	}
 	/**
 	 * Login.
 	 *
@@ -52,22 +38,25 @@ public class Login {
 	 */
 	public String login() {
 		LOGGER.debug("login!!");
-		LOGGER.debug("username: {}", user.getAccount());
+		LOGGER.debug("username: {}");
 		LOGGER.debug("password: {}", user.getPassword());
+		
+		System.out.println(user.getPassword());
+		System.out.println(user.getAccount());
 
 		
 
 		boolean userExist = userExist(user);
 		FacesContext context = FacesContext.getCurrentInstance();
 	    HttpSession session = (HttpSession) context.getExternalContext().getSession(true);
-	    String checkCode  =(String ) session.getAttribute("check_code");
+//	    String checkCode  =(String ) session.getAttribute("check_code");
 	    
-	    LOGGER.debug("checkCode : {}",checkCode);
-	    
-	    boolean ok = StringUtils.equals(checkCode, checkout);
-		if (userExist && ok ) {
+//	    LOGGER.debug("checkCode : {}",checkCode);
+	 
+		if (userExist) {
+			LOGGER.debug("成功");
 			this.message = "";
-			return "success";
+			return "admin-services.xhtml";
 		} else {
 			LOGGER.debug("帳號密碼錯誤，請重新登入");
 			this.message = "帳號密碼錯誤，請重新登入";
@@ -91,20 +80,6 @@ public class Login {
 		} else {
 			return false;
 		}
-	}
-
-	/**
-	 * Logout.
-	 *
-	 * @return the string
-	 */
-	public String logout() {
-		LOGGER.debug("login!!");
-		LOGGER.debug("username: {}", user.getAccount());
-		LOGGER.debug("password: {}", user.getPassword());
-		
-		
-		return null;
 	}
 
 	/**
@@ -143,14 +118,6 @@ public class Login {
 	 */
 	public void setMessage(String message) {
 		this.message = message;
-	}
-
-	public String getCheckout() {
-		return checkout;
-	}
-
-	public void setCheckout(String checkout) {
-		this.checkout = checkout;
 	}
 
 }
