ee20d79d1c7f3de74bdedade82dc8818647a3a7d
cat <cat@192.168.5.113>
2016-08-31 15:28:28 +0800

    demo

--- src/main/java/org/greenpeace/bean/Customer.java
+++ src/main/java/org/greenpeace/bean/Customer.java
@@ -21,7 +21,7 @@ public class Customer implements Serializable {
 	@Column(name="order_id")
 	private int orderId;
 	
-	private int order_id;
+	
 	
 	@Column(name="member_account")
 	private String memberAccount;
@@ -156,13 +156,6 @@ public class Customer implements Serializable {
 		this.itemPrice = itemPrice;
 	}
 	
-	public int getOrder_id() {
-		return order_id;
-	}
 
-	public void setOrder_id(int order_id) {
-		this.order_id = order_id;
-	}
-	
 
 }
