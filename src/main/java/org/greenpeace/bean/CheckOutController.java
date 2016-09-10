cb7b45125b1d2751d73fd07c50a939d674a6a41f
cat <cat@192.168.7.82>
2016-09-10 16:19:52 +0800

    e

---
+++ src/main/java/org/greenpeace/bean/CheckOutController.java
@@ -0,0 +1,17 @@
+package org.greenpeace.bean;
+
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+
+@ManagedBean(name="ckoutmoney")
+@RequestScoped
+public class CheckOutController implements Serializable{
+private static final long serialVersionUID = 855018494784719093L;
+
+
+}
--- src/main/java/org/greenpeace/service/CustomerServicesImpl.java
+++ src/main/java/org/greenpeace/service/CustomerServicesImpl.java
@@ -1,11 +1,17 @@
 package org.greenpeace.service;
 
 import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
 import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.beanutils.PropertyUtils;
 import org.greenpeace.bean.Customer;
 import org.greenpeace.bean.CustomerDto;
+import org.greenpeace.bean.Member;
 import org.greenpeace.bean.ProductDto;
 import org.greenpeace.dao.CustomerDAO;
 import org.greenpeace.dao.ItemDAO;
@@ -13,6 +19,7 @@ import org.greenpeace.dao.OrderDAO;
 import org.greenpeace.dao.ProductDAO;
 import org.greenpeace.dao.RestaurantDAO;
 import org.greenpeace.dao.UserDAO;
+import org.greenpeace.dao.UserDAOImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,9 +29,9 @@ public class CustomerServicesImpl implements CustomerService {
 	private ItemDAO itemDao;
 	private UserDAO userDao;
 	private OrderDAO orderDao;
-//	private ProductDAO productDao;
-//	private RestaurantDAO restaurantDao;
-//	private 
+	private ProductDAO productDao;
+	private RestaurantDAO restaurantDao;
+ 
 
 	@Override
 	public boolean judgeExist(CustomerDto dto) {
@@ -67,21 +74,46 @@ public class CustomerServicesImpl implements CustomerService {
 		}
 	}
 	
+	
+	
+	
+	
 	@Override
 	public CustomerDto logic(List<ProductDto> pro) {
 		CustomerDto result =null;
+		int total1=0;
+		BigDecimal total = new BigDecimal(0);
 		//TODO  還要判斷金額
 		for(ProductDto unit : pro){
 			if(unit.getNumber() > 0){
-				logger.debug("product namr: {}",unit.getData().getName());
+				logger.debug("product name: {}",unit.getData().getName());
+				logger.debug("product price: {}",unit.getData().getPrice());
+				logger.debug("product number {}",unit.getNumber());
+				int money= unit.getData().getPrice()*unit.getNumber();
+				 total1=total1+money;
+				logger.debug(Integer.toString(money));
+				
+				
+				 
 				if( result ==null ){
 					result =new CustomerDto();
 					result.setOk(true);
 				}
 			}
+			
 		}
-		
+		logger.debug(Integer.toString(total1));
+		total = new BigDecimal(total1);
+		HttpServletRequest request= (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+				.getRequest();
+		BigDecimal cash = new BigDecimal( String.valueOf(request.g));
+        
+		System.out.println(cash);
 		return result;
+	}
+	private HttpSession getSession() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
